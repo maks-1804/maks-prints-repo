a5839ff21305b27364409020408254f68d6d62e4
Kaitlin Schaer <kaitlin.schaer@gmail.com>
2018-06-04 22:05:59 -0400

    changed eslintrc so no semicolons is enforced, prettier formatting should be ok now

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -23,7 +23,6 @@ Category.belongsToMany(Product)
 // Product.hasMany(Review)
 // Review.belongsTo(Product)
 
-
 /**
  * We'll export all of our models here, so that any time a module needs a model,
  * we can just require it from 'db/models'
