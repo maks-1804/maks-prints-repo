0b4c19e85885c31c0c6de498f1c7dd06caae37dc
Arielle Gordon <reallygordon@gmail.com>
2018-06-05 14:54:25 -0400

    all routes for products completed, but not with admin privileges specified

--- server/api/products.js
+++ server/api/products.js
@@ -5,7 +5,7 @@ module.exports = router
 //GET /api/products/ --- all products
 router.get('/', async (req, res, next) => {
   try {
-    const products = Product.findAll({include: {all: true}})
+    const products = await Product.findAll({include: {all: true}})
     if (!products) { res.sendStatus(404) }
     res.json(products)
   }
@@ -18,7 +18,7 @@ router.get('/')
 //GET /api/products/:id --- single product
 router.get('/:id', async (req, res, next) => {
   try {
-    const product = Product.findById(req.params.id)
+    const product = await Product.findById(req.params.id)
     if (!product) { res.sendStatus(404) }
     res.json(product)
   }
@@ -29,15 +29,32 @@ router.get('/:id', async (req, res, next) => {
 
 //POST /api/products --- new product
 router.post('/', async (req, res, next) => {
-
+// awaiting further discussion of admin validation
+  try {
+    const product = await Product.create(req.body)
+    const productWithAssociations = await Product.findById(product.id, {include: {all: true}})
+    res.json(productWithAssociations)
+  }
+  catch (err) { next(err) }
 })
 
 //PUT /api/products/:id --- edit product
 router.put('/:id', async (req, res, next) => {
-
+  try {
+    const product = await Product.findById(req.params.id, {include: {all: true}});
+    if (!product) { res.sendStatus(404) }
+    const updated = await product.update(req.body);
+    res.json(updated)
+  }
+  catch (err) { next(err) }
 })
 
 //DELETE /api/products/:id --- delete product
 router.delete('/:id', async (req, res, next) => {
-
+  try {
+    const product = await Product.findById(req.params.id);
+    await product.destroy();
+    res.status(204).end()
+  }
+  catch (err) { next(err) }
 })
