40639d54f1c4ed9e4bbeb4efeebf614199a9d947
Arielle Gordon <reallygordon@gmail.com>
2018-06-05 14:40:17 -0400

    added get all and get by id product routes

--- server/api/products.js
+++ server/api/products.js
@@ -4,7 +4,12 @@ module.exports = router
 
 //GET /api/products/ --- all products
 router.get('/', async (req, res, next) => {
-
+  try {
+    const products = Product.findAll({include: {all: true}})
+    if (!products) { res.sendStatus(404) }
+    res.json(products)
+  }
+  catch (err) { next(err) }
 })
 
 //GET /api/products/?query --- products in a category
@@ -12,7 +17,12 @@ router.get('/', async (req, res, next) => {
 
 //GET /api/products/:id --- single product
 router.get('/:id', async (req, res, next) => {
-
+  try {
+    const product = Product.findById(req.params.id)
+    if (!product) { res.sendStatus(404) }
+    res.json(product)
+  }
+  catch (err) { next(err) }
 })
 
 //   ---ADMIN ONLY---
