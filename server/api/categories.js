6ce525388cca12850ef8a261e64ce832f649c40f
Kaitlin Schaer <kaitlin.schaer@gmail.com>
2018-06-04 22:41:30 -0400

    added route for get on a single category

--- server/api/categories.js
+++ server/api/categories.js
@@ -1,12 +1,22 @@
-const router = require('express').Router();
-const { Category } = require('../db/models');
-module.exports = router;
+const router = require('express').Router()
+const { Category } = require('../db/models')
+module.exports = router
 
 router.get('/', (req, res, next) => {
   try {
-    const categories = Category.findAll();
-    res.json(categories);
+    const categories = Category.findAll()
+    res.json(categories)
   } catch (error) {
-    next(error);
+    next(error)
   }
-});
+})
+
+router.get('/:id', (req, res, next) => {
+  try {
+    const category = Category.findById(req.params.id)
+    if (!category) res.sendStatus(404)
+    res.json(category)
+  } catch (error) {
+    next(error)
+  }
+})
