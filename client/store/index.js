81ac952a54e8d63fcf6624f429638a791c1809f5
Monica Gonzalez <mogon2990@gmail.com>
2018-06-06 23:06:05 -0400

    review reducer, some issues to review before proceeding

--- client/store/index.js
+++ client/store/index.js
@@ -3,8 +3,10 @@ import createLogger from 'redux-logger'
 import thunkMiddleware from 'redux-thunk'
 import {composeWithDevTools} from 'redux-devtools-extension'
 import user from './user'
+import products from './products'
+import reviews from './reviews'
 
-const reducer = combineReducers({user})
+const reducer = combineReducers({user, products, reviews})
 const middleware = composeWithDevTools(applyMiddleware(
   thunkMiddleware,
   createLogger({collapsed: true})
---
+++ client/store/reviews.js
@@ -0,0 +1,100 @@
+import axios from 'axios'
+import history from '../history'  // we'll see if we need this in the thunks
+
+/* will discuss - add a thunk for GET /api/reviews/user/:userId ?? */
+
+/**
+ * ACTION TYPES
+ */
+const GET_REVIEWS = 'GET_REVIEWS'
+const ADD_NEW_REVIEW = 'ADD_NEW_REVIEW'
+const EDIT_REVIEW = 'EDIT_REVIEW'
+const DELETE_REVIEW = 'DELETE_REVIEW'
+
+/**
+ * ACTION CREATORS
+ */
+const getReviews = reviews => ({type: GET_REVIEWS, reviews})
+const addNewReview = review => ({type: ADD_NEW_REVIEW, review})
+const editReview = review => ({type: EDIT_REVIEW, review})
+const deletedReview = reviewId => ({type: DELETE_REVIEW, reviewId})
+
+/**
+ * THUNK CREATORS
+ */
+
+ export const loadAllReviews = () => {
+    return async (dispatch) => {
+      try {
+        const reviews = await axios.get('/api/reviews').data
+        dispatch(getReviews(reviews))
+      } catch (err) {
+        console.log('Error getting all reviews: ', err.message)
+      }
+    }
+ }
+
+ export const addReview = review => {
+   return async (dispatch) => {
+     try {
+       const newReview = await axios.post('/api/reviews', review).data
+       dispatch(addNewReview(newReview))
+     } catch (err) {
+       console.log('Error adding new review: ', err.message)
+     }
+   }
+ }
+
+ export const updateReview = review => {
+   return async (dispatch) => {
+     try {
+        const updatedReview = await axios.put(`/api/reviews/${review.id}`, review).data
+        dispatch(editReview(updatedReview))
+     } catch (err) {
+       console.log('Error editing review: ', err.message)
+     }
+   }
+ }
+
+
+/* needs review with api review routes */
+
+//  export const deleteReview = reviewId => {
+//    return async (dispatch) => {
+//      try {
+//         const deleteThisReview = await axios.delete(`/api/reviews/products/${productId}`) //??
+//         dispatch(deletedReview(reviewId))
+//      } catch (err) {
+//        console.log('Error deleting review: ', err.message)
+//      }
+//    }
+//  }
+
+/**
+ * INITIAL STATE
+ */
+const defaultReviews = []
+
+/**
+ * REDUCER
+ */
+
+export default (state = defaultReviews, action) => {
+  switch (action.type) {
+    case GET_REVIEWS:
+      return action.reviews
+    case ADD_NEW_REVIEW:
+      return [...state, action.review]
+    case EDIT_REVIEW:
+      return state.map( review => {
+        if (review.id === action.review.id) {
+          return action.review    //if the ids match, maps keeps updated
+        }
+        return review             //otherwise, map keeps as is
+      })
+    // case DELETE_REVIEW
+      //pending discussion
+    default:
+      return state
+  }
+}
