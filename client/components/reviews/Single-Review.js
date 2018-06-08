764833bf04d9eb39917c442af8ffe06a7654d3eb
Kaitlin Schaer <kaitlin.schaer@gmail.com>
2018-06-08 18:08:36 -0400

    got review components working on the single product pages

--- client/components/reviews/Single-Review.js
+++ client/components/reviews/Single-Review.js
@@ -7,7 +7,7 @@ const SingleReview = props => {
   return (
     <div className="container-review-single">
       <h4>{review.title}</h4>
-      <h5>{`review by user: ${review.userId} at ${review.createdAt}`}</h5>
+      <h5>{`by user: ${review.userId} at ${review.createdAt}`}</h5>
       <p>{review.content}</p>
     </div>
   )
