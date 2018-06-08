dd128a3bf720812eb06261fa0bd3102b1e4655c5
Kaitlin Schaer <kaitlin.schaer@gmail.com>
2018-06-08 15:14:37 -0400

    single product now loads even with page refresh

--- client/components/shop/Product-Single.js
+++ client/components/shop/Product-Single.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
-
+import { loadAllProducts } from '../../store/products'
 
 class SingleProduct extends Component {
   constructor() {
@@ -11,6 +11,13 @@ class SingleProduct extends Component {
     }
     this.handleChange = this.handleChange.bind(this)
     this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  componentDidMount() {
+    if (!this.props.product) {
+      console.log('dispatching!')
+      this.props.fetchProducts()
+    }
   }
 
   handleSubmit(event) {
@@ -25,48 +32,78 @@ class SingleProduct extends Component {
   }
 
   render() {
-    const { id, title, description, price, inventoryQuantity, imageUrl } = this.props.location.props
-    console.log('props: ', this.props.location.props)
+    const { product } = this.props
+    // const title = product.title
+    // const description = product.description
+    // const price = product.price
+    // const inventoryQuantity = product.inventoryQuantity
+    // const imageUrl = product.imageUrl
+    //let title, description, price, inventoryQuantity, imageUrl
+    let quantity = 0
+    console.log('props: ', this.props)
+    let productContent
+    let price = 0
+    if (product) {
+      quantity = product.inventoryQuantity
+      price = product.price
+      productContent = (
+        <div>
+          <img src={product.imageUrl} />
+          {/* </div> */}
+          {/* <div className='single-prod-child'> */}
+          <h1>{product.title}</h1>
+          <p>{product.description}</p>
+          <h5>${product.price}</h5>
+        </div>
+      )
+    }
+    const loadingContent = (
+      <div>
+        <p>loading this product...</p>
+      </div>
+    )
     return (
       // classNames can be changed...flexbox in mind (2columns, pic vs everything else)
       //if it works better with something (bootstrap?), feel free to change classNames/div structure!
-      <div className='container-single-prod-parent'>
+      <div className="container-single-prod-parent">
         {/* <div className='single-prod-child'> */}
-          <img src={imageUrl} />
+        {product ? productContent : loadingContent}
+        <form>
+          <label>Select Quantity</label>
+          <select
+            name="product-quantity"
+            value={this.state.selectedQuantity}
+            onChange={this.handleChange}
+          >
+            {Array.apply(null, new Array(quantity)).map((el, ind) => {
+              return <option key={ind}>{ind}</option>
+            })}
+          </select>
+        </form>
+        <h5>Subtotal: ${this.state.selectedQuantity * price}</h5>
+        <button type="submit" onClick={this.handleSubmit}>
+          Add to cart
+        </button>
         {/* </div> */}
-        {/* <div className='single-prod-child'> */}
-          <h1>{title}</h1>
-          <p>{description}</p>
-          <h5>${price}</h5>
-          <form>
-            <label>Select Quantity</label>
-            <select
-              name='product-quantity'
-              value={this.state.selectedQuantity}
-              onChange={this.handleChange}>
-              {Array.apply(null, new Array(inventoryQuantity)).map((el, ind) => {
-                  return (<option key={ind}>{ind}</option>)
-                })}
-            </select>
-          </form>
-          <h5>Subtotal: ${this.state.selectedQuantity * price}</h5>
-          <button type='submit' onClick={this.handleSubmit}>Add to cart</button>
-        {/* </div> */}
-        <div>
-          {/* REVIEWS */}
-        </div>
+        <div>{/* REVIEWS */}</div>
       </div>
     )
   }
 }
-
-const mapDispatch = dispatch => {
+const mapStateToProps = (state, ownProps) => {
+  const id = Number(ownProps.match.params.productId)
   return {
-    test: () => dispatch('thing')
+    product: state.products.filter(product => product.id === id)[0],
+    products: state.products
+  }
+}
+const mapDispatchToProps = dispatch => {
+  return {
+    fetchProducts: () => dispatch(loadAllProducts())
   }
 }
 
-export default connect(null, mapDispatch)(SingleProduct)
+export default connect(mapStateToProps, mapDispatchToProps)(SingleProduct)
 
 // SingleProduct.propTypes = {
 //   id: PropTypes.string.isRequired,
