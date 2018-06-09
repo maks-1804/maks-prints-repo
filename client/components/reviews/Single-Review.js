295280867176aa91aa06df20b7864442a02ddc60
Kaitlin Schaer <kaitlin.schaer@gmail.com>
2018-06-09 11:06:52 -0400

    now we have access to the moment.js library for date parsing

--- client/components/reviews/Single-Review.js
+++ client/components/reviews/Single-Review.js
@@ -1,13 +1,26 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import moment from 'moment'
 
 const SingleReview = props => {
+  //helper function to show a relative date- eg, "today" "two months ago" or "two years ago"
   const { review } = props
-  console.log('the review:', review)
+  const timeSince = moment(review.createdAt).fromNow()
+  // const now = new Date()
+  // const rev = new Date(review.createdAt)
+  // console.log(review.createdAt)
+  // const diff = now - rev
+  // console.log(
+  //   `review left at ${rev} vs now ${now} and diff: ${diff / 1000}, ${diff /
+  //     1000 /
+  //     (60 * 60 * 24)}, type: ${typeof review.createdAt}`
+  // )
+
+  console.log('the review:', review, timeSince)
   return (
     <div className="container-review-single">
       <h4>{review.title}</h4>
-      <h5>{`by user: ${review.userId} at ${review.createdAt}`}</h5>
+      <h6>{`review by user: ${review.userId}, ${timeSince}`}</h6>
       <p>{review.content}</p>
     </div>
   )
