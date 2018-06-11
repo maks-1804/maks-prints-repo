7e2a3ca53de15e167e5974ffd2f295bca1834338
Sharon Yun <sharonyun11@gmail.com>
2018-06-11 16:21:58 -0400

    Admin Dashboard skeleton with product management

---
+++ client/components/admin/Admin-Dashboard.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import { connect } from 'react-redux'
+
+import { loadAllProducts, deleteProduct } from '../../store/products'
+import { ProductCard } from '../shop'
+
+class AdminDashboard extends React.Component {
+  componentDidMount() {
+    this.props.fetchProducts()
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <div>
+          <h3>Product Management</h3>
+          <div>
+            {
+              this.props.products.map(product => (
+                <ProductCard key={product.id} product={product} isAdmin={this.props.isAdmin} deleteProduct={this.props.deleteProduct} />
+              ))
+            }
+          </div>
+        </div>
+
+        <div>
+          <h3>Order Management</h3>
+        </div>
+
+        <div>
+          <h3>User Management</h3>
+
+        </div>
+      </div>
+    )
+  }
+}
+
+const mapState = (state) => {
+  return {
+    products: state.products,
+    isAdmin: !!state.user.isAdmin
+  }
+}
+
+const mapDispatch = (dispatch) => {
+  return {
+    fetchProducts: () => dispatch(loadAllProducts()),
+    deleteProduct: (id) => dispatch(deleteProduct(id))
+
+  }
+}
+
+export default connect(mapState, mapDispatch)(AdminDashboard)
