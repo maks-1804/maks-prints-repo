5a35543e39bb91410179e2d23d60a67faa5560b9
Monica Gonzalez <mogon2990@gmail.com>
2018-06-12 15:46:55 -0400

    fixing read of ENV variabls for google oauth

--- server/auth/google.js
+++ server/auth/google.js
@@ -3,7 +3,8 @@ const router = require('express').Router()
 const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy
 const { User } = require('../db/models')
 module.exports = router
-const { clientId, clientSecret, clientCallbackURL } = require('../../secrets')
+
+require('../../secrets')
 
 /**
  * For OAuth keys and other secrets, your Node process will search
@@ -26,9 +27,9 @@ if (!process.env.GOOGLE_CLIENT_ID || !process.env.GOOGLE_CLIENT_SECRET) {
 } else {
 
   const googleConfig = {
-    clientID: clientId,
-    clientSecret: clientSecret,
-    callbackURL: clientCallbackURL
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: process.env.GOOGLE_CALLBACK
   }
 
   const strategy = new GoogleStrategy(googleConfig, (token, refreshToken, profile, done) => {
