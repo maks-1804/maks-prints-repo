e2be0e77f9f5af44b1384490df7ee54aceeb5321
Monica Gonzalez <mogon2990@gmail.com>
2018-06-12 16:01:54 -0400

    fixed google thing

--- server/auth/google.js
+++ server/auth/google.js
@@ -22,7 +22,7 @@ require('../../secrets')
 
 if (!process.env.GOOGLE_CLIENT_ID || !process.env.GOOGLE_CLIENT_SECRET) {
 
-  console.log('Google client ID / secret not found. Skipping Google OAuth.')
+  console.log('Google client ID/secret not found. Skipping Google OAuth.')
 
 } else {
 
