c0c5f25cf156ed32626b69561f9a3fa5ef550fbd
Monica Gonzalez <mogon2990@gmail.com>
2018-06-12 15:36:41 -0400

    added Link to product title

--- client/components/cart/Cart-ProductCard.js
+++ client/components/cart/Cart-ProductCard.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
 
 const CartProductCard = props => {
   const { product, cartStatus, changeQuantity, deleteItem } = props
